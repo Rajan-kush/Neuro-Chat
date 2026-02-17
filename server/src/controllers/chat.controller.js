e2219f01734be471355ba2cefaefeeeb9a1edc61
Rajan Kumar <rajankrkushwaha07@gmail.com>
2026-02-17 14:08:53 +0530

    add chat controller with persona context and message history

---
+++ server/src/controllers/chat.controller.js
@@ -0,0 +1,80 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { openai } from "../utils/geminiApi.js";
+import { Chat } from "../models/chat.model.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { Convo } from "../models/convo.model.js";
+
+const chatWithPersona = asyncHandler(async (req, res) => {
+  const { userMessage } = req.body;
+  const { convoId } = req.params;
+
+  const convo = await Convo.findById(convoId).populate("personaId");
+
+  if (!convo) {
+    throw new ApiError(404, "No conversation found");
+  }
+
+  const chatHistory = await Chat.find({ conversationId: convo._id })
+    .select("role content -_id")
+    .sort({ createdAt: 1 });
+
+  const persona = convo.personaId;
+
+  let messages = [
+    {
+      role: "system",
+      content: persona.description,
+    },
+  ];
+
+  if (chatHistory && chatHistory.length > 0) {
+    messages.push(...chatHistory);
+  }
+  messages.push({
+    role: "user",
+    content: userMessage,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gemini-2.5-flash",
+    messages,
+  });
+
+  const answer = response.choices[0].message.content;
+
+  if (!answer) {
+    throw new ApiError(500, "something went wrong while generating answer");
+  }
+
+  const userChat = await Chat.create({
+    role: "user",
+    content: userMessage,
+    conversationId: convo._id,
+    personaId: convo.personaId,
+    userId: req.user._id,
+  });
+
+  const assistantChat = await Chat.create({
+    role: "assistant",
+    content: answer,
+    conversationId: convo._id,
+    personaId: convo.personaId,
+    userId: req.user._id,
+  });
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      user: {
+        role: userChat.role,
+        content: userChat.content,
+      },
+      assistant: {
+        role: assistantChat.role,
+        content: assistantChat.content,
+      },
+    })
+  );
+});
+
+export { chatWithPersona };
--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { newConversation } from "../controllers/convo.controller.js";
+import { chatWithPersona } from "../controllers/chat.controller.js";
 import { jwtVerify } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/convo").post(jwtVerify, newConversation);
+router.route("/:convoId").post(jwtVerify, chatWithPersona);
 
 export default router;
